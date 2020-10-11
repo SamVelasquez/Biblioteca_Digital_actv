03fab344d80094eb9bdb68346d33dc0506fd88bb
stomerger <63244668+stomerger@users.noreply.github.com>
2020-10-11 18:55:29 -0300

    Initial commit

---
+++ app/src/main/java/com/example/biblioteca_digital/Github_act.java
@@ -0,0 +1,46 @@
+package com.example.biblioteca_digital;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class Github_act extends AppCompatActivity {
+    private Spinner spinner;
+    private TextView text1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_github_act);
+        //declaro el texview
+        text1=(TextView)findViewById(R.id.text1);
+        //declaro el sppiner
+        spinner = (Spinner) findViewById(R.id.spinner);
+        //creo el array del spinner
+        String[] NombreLibros = {" ", "Farenheith", "Revival", "El Alquimista"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, NombreLibros);
+        spinner.setAdapter(adapter);// asocio el spinner con el array
+    }
+    public void opciones(View view) {
+        //precios originales
+        String opcion = spinner.getSelectedItem().toString();
+        if (opcion.equals("Farenheith")) {
+            int proucto1 = 50000;
+            text1.setText(" valor total: "+ proucto1);
+        }
+        else if (opcion.equals("El Alquimista")) {
+            int proucto1 = 120000;
+            text1.setText(" valor total: "+ proucto1);
+        }
+        else if (opcion.equals("Revival")) {
+            int proucto1 = 450000;
+            text1.setText(" valor total: "+proucto1);
+        }else
+            text1.setText(":v");
+    }
+}
--- app/src/main/java/com/example/biblioteca_digital/MainActivity.java
+++ app/src/main/java/com/example/biblioteca_digital/MainActivity.java
@@ -2,13 +2,66 @@ package com.example.biblioteca_digital;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+    private ProgressBar pgbar;
+    private Button btton1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //button
+        btton1 = (Button)findViewById(R.id.btton);
+        // llamo al identificador para poder usar el boton mediante codigo
+                 btton1.setOnClickListener(new View.OnClickListener() {
+                  @Override
+                  public void onClick(View view) {
+                    new task().execute();
+                  }
+                 });
+        //progresbar
+        pgbar=(ProgressBar)findViewById(R.id.pgbar);
+        //coloco el progres var de inicio invisible
+        pgbar.setVisibility(View.INVISIBLE);
+
+
     }
+    // se crea la activida asincronica (progresbar)
+    class task extends AsyncTask<String, Void,String> {
+
+        @Override// aqui configuracion inicial de barra de progreso
+        protected void onPreExecute() { // inicio
+            pgbar.setVisibility(View.VISIBLE);
+        }
+        @Override //en esta parte se corren las tareas pesadas
+        protected String doInBackground(String... strings) { //desarollo
+            for( int i= 1; i<=10; i++){
+                try { // ejecuta esta sentencia por milmilesegundo
+                    Thread.sleep(1000);
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String s) {//final
+            pgbar.setVisibility(View.INVISIBLE  );//desaparesco el progresbar
+            Intent i = new Intent(getApplicationContext(),Github_act.class);
+            startActivity(i);
+        }
+    }
+
 }
